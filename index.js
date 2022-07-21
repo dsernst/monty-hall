f2a3c8bd69ac530ed5a62e47180a7d961137b418
David Ernst <git@dsernst.com>
2022-07-21 14:24:46 -0700

    Code cleanup

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const games = 10000
 let origWon = 0
 let switchWon = 0
 
+const pickOneOfThree = () => Math.floor(Math.random() * 3)
+
 for (let i = 0; i < games; i++) {
-  const correct = Math.floor(Math.random() * 3)
-  const orig = Math.floor(Math.random() * 3)
+  const correct = pickOneOfThree()
+  const orig = pickOneOfThree()
 
-  let revealed = Math.floor(Math.random() * 3)
-  while (revealed === orig || revealed === correct) {
-    revealed = Math.floor(Math.random() * 3)
-  }
+  // Revealed is never the correct answer, nor the original pick
+  let revealed = pickOneOfThree()
+  while (revealed === orig || revealed === correct) revealed = pickOneOfThree()
 
+  // Switched is to the one door remaining
   const switched = [0, 1, 2].find((x) => x !== orig && x !== revealed)
 
+  // Count wins
   if (orig === correct) origWon++
   if (switched === correct) switchWon++
 }
